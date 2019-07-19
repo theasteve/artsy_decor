6c211712d30f15f2e9f86257c45549cdfe716716
theasteve <stevenaguilarcs@gmail.com>
2019-07-18 20:51:33 -0400

    Sign out link on navigation bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_one_attached :avatar
+
+  before_create :set_defaults
+
+  def set_defaults
+    self.avatar = 'assets/images/astronaut.svg' if self.new_record?
+  end
 end
