e207d74eeac1ac08fd9e3f689c74f59603a3b456
theasteve <stevenaguilarcs@gmail.com>
2019-07-27 00:00:39 -0400

    Create a new question and display it

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,52 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.create(question_params)
+    binding.pry
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(questions_params)
+      redirect_to @question, notice: 'Question was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy!
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(
+      :user_id,
+      :title,
+      :content
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_014454) do
+ActiveRecord::Schema.define(version: 2019_07_23_024554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +66,15 @@ ActiveRecord::Schema.define(version: 2019_06_12_014454) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,4 +92,5 @@ ActiveRecord::Schema.define(version: 2019_06_12_014454) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "questions", "users"
 end
