576fd681eebc730ec1ba9121a763cac65bc76dd0
theasteve <stevenaguilarcs@gmail.com>
2020-04-10 19:20:24 -0400

    Create script to display name of file when upload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,13 +10,11 @@ require("channels")
 require("stylesheets/application.scss")
 require("trix")
 require("@rails/actiontext")
-
+require("packs/posts")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-//= require posts.js
 
--- app/javascript/packs/posts.js
+++ app/javascript/packs/posts.js
@@ -1,9 +1,7 @@
 window.onload = function() {
-  console.log('PARTTYY')
   const fileUploadElement = document.getElementById('file-upload');
-  console.log('HEREEE');
+
   if(fileUploadElement) {
-    debugger;
     fileUploadElement.addEventListener('change', function(){
       const fileName = document.getElementById("post_image").files[0].name
       const fileNameElement = document.getElementById('file-name');
