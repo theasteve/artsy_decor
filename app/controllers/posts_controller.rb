a911777fe0f5c973914982dabcda417bdbaba813
STEVEN AGUILAR <stevenaguilarcs@gmail.com>
2020-04-09 20:48:21 -0400

    Delete edit post (#31)
    * Configure Cloudinary with ActiveStorage
    
    * Add styles to display blob attachements
    
    * Add gitignore file
    
    * Create Sign Up form and installed dotenv
    
    * Refactor nav bar with a settings icon
    
    * Add delete and edit icons to the post template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+
+    redirect_to root_path
+  end
+
   private
 
   def post_params
